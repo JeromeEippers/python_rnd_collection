e7b2d6ed4c2f8a3b7752a84589eaecd67838ed82
Jerome Eippers <jerome.eippers@ubisoft.com>
2019-11-07 20:53:32 +0100

    structure for the motiongraph notebook

---
+++ new_challenge_old_research/animation.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pickle
+
+
+def load_animation(path):
+    """
+    Return an animation as :
+    ( keycount,
+        [(bonename, [np.matrix, np.matrix, ...]), () ...]
+    )
+    """
+    
+    animation = pickle.load(open(path,'rb'))
+    
+    #convert matrix in numpy matrices
+    #format of the picle is just a list of tuples with :
+    #[ (bonename, [matrix, matrix, ...]), () ]
+    
+    tracks = []
+    for track in animation:
+        keys = [np.array([
+                    m[0:4],
+                    m[4:8],
+                    m[8:12],
+                    m[12:16]]) for m in track[1]]
+        tracks.append((track[0], keys))
+        
+    return (len(tracks[0][1]), tracks)
+    
+    
--- new_challenge_old_research/skeleton.py
+++ new_challenge_old_research/skeleton.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 
 class DataBone(object):
     
@@ -28,3 +29,17 @@ class DataSkeleton(object):
         if self._bones[boneId]._parentId >= 0:
             return np.dot(self._bones[boneId]._matrix, self.globalMatrix(self._bones[boneId]._parentId))
         return self._bones[boneId]._matrix.copy()
+    
+    def load_animation(self, animation, frame):
+        keycount, tracks = animation
+        for track in tracks:
+            name = track[0]
+            bone = next((bone for bone in self._bones if bone._name == name),None)
+            if bone:
+                bone._matrix = track[1][frame]
+    
+    
+def load_skeleton(path):
+    skeleton = pickle.load(open(path,'rb'))
+    skeleton.convert_to_np()
+    return skeleton
