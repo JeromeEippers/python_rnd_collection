0bbb845e72220746ea0996ba9dcd80c3b9e920d6
Jerome Eippers <jerome.eippers@ubisoft.com>
2020-11-13 00:23:45 +0100

    deep q learning with fixed target

---
+++ deep_learning/deep_q_learning_with_target.py
@@ -0,0 +1,154 @@
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import gym
+
+class DeepQNetwork(nn.Module):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
+        super(DeepQNetwork, self).__init__()
+
+        if T.cuda.is_available() == False:
+            raise Exception('no cuda')
+
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cuda:0')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        actions = self.fc3(x)
+        return actions
+
+
+class Agent(object):
+    def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size=100000,
+                 eps_min=0.01, eps_dec=0.996, target_iteration=200):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.eps_min = eps_min
+        self.eps_dec = eps_dec
+        self.batch_size = batch_size
+        self.n_actions = n_actions
+        self.action_space = [i for i in range(n_actions)]
+        self.mem_size = max_mem_size
+        self.mem_cntr = 0
+        self.target_iteration = target_iteration
+        self.target_iteration_cntr = 0
+
+        self.Q_eval = DeepQNetwork(lr, n_actions=self.n_actions, input_dims=input_dims, fc1_dims=256, fc2_dims=256)
+        self.Q_target = DeepQNetwork(lr, n_actions=self.n_actions, input_dims=input_dims, fc1_dims=256, fc2_dims=256)
+
+        self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
+        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
+
+    def store_transition(self, state, action, reward, state_, terminal):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = terminal
+        self.new_state_memory[index] = state_
+        self.mem_cntr += 1
+
+    def choose_action(self, observation):
+        rand = np.random.random()
+        if rand < self.epsilon:
+            action = np.random.choice(self.action_space)
+        else:
+            state = T.tensor([observation]).to(self.Q_eval.device)
+            actions = self.Q_eval.forward(state)
+            action = T.argmax(actions).item()
+        return action
+
+    def learn(self):
+        if self.mem_cntr > self.batch_size:
+            self.Q_eval.optimizer.zero_grad()
+
+            if self.target_iteration_cntr % self.target_iteration == 0:
+                self.Q_target.load_state_dict(self.Q_eval.state_dict())
+            self.target_iteration_cntr += 1
+
+            max_mem = min(self.mem_size, self.mem_cntr)
+            batch = np.random.choice(max_mem, self.batch_size, replace=False)
+            batch_index = np.arange(self.batch_size, dtype=np.int32)
+
+            state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
+            reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
+            terminal_batch = T.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
+            new_state_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
+            action_batch = self.action_memory[batch]
+
+            q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
+            q_next = self.Q_target.forward(new_state_batch)
+            q_next[terminal_batch] = 0.0
+
+            q_target = reward_batch + self.gamma * T.max(q_next, dim=1)[0]
+
+            loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
+            loss.backward()
+            self.Q_eval.optimizer.step()
+
+            self.epsilon = self.epsilon - self.eps_dec
+            self.epsilon = max(self.epsilon, self.eps_min)
+
+
+if __name__ == '__main__':
+    env = gym.make('LunarLander-v2')
+    brain = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4, input_dims=[8], lr=0.001, eps_dec=0.00003)
+
+    scores = []
+    eps_history = []
+    n_games = 500
+    score = 0
+
+    for i in range(n_games):
+        if i%10 == 0 and i > 0:
+            avg_score = np.mean(scores[max(0, i-10):(i+1)])
+            print('episode', i, 'average score {}'.format(avg_score), 'epsilon {}'.format(brain.epsilon))
+        else:
+            print('episode', i, 'score', score)
+
+        score = 0
+        eps_history.append(brain.epsilon)
+        observation = env.reset()
+        done = False
+        while not done:
+            action = brain.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            score += reward
+            brain.store_transition(observation, action, reward, observation_, done)
+            brain.learn()
+            observation = observation_
+        scores.append(score)
+
+    plt.plot(scores)
+    plt.savefig('deep_q_learning_with_target.png')
+
+    # play the game a few time
+    for i in range(10):
+        observation = env.reset()
+        done = False
+        while not done:
+            action = brain.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            observation = observation_
+
+            env.render()
+        scores.append(score)
